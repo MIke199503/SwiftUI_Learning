fc88ab69fc2afeb80e0b9107131b4c6967ab2168
Mike199503 <zth19950326@yahoo.com>
2020-12-12 21:11:16 +0800

    2020-12-12提交～SDWebImageSwiftUI的使用

--- DesignCode/CourseStore.swift
+++ DesignCode/CourseStore.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Contentful
 
+
 let client  = Client(spaceId: "0ge8xzmnbq2c", accessToken: "03010b0d79abcb655ca3fda453f2f493b5472e0aaa53664bc7dea5ef4fce2676" )
 
 func getArray(id:String,completion:@escaping([Entry]) -> ()) {
@@ -32,14 +33,18 @@ func getArray(id:String,completion:@escaping([Entry]) -> ()) {
 class CourseStore:ObservableObject{
     @Published var course:[Course] = courseData
     init() {
+        
+            let colors = [#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1),#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1),#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1),#colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1),#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1),#colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1),#colorLiteral(red: 0.721568644, green: 0.8862745166, blue: 0.5921568871, alpha: 1)]
+        
+        
         getArray(id: "course") { (items) in
             items.forEach { (item) in
                 self.course.append(Course(
                                     title: item.fields["title"] as! String,
                                     subtitle: item.fields["subtitle"] as! String,
-                                    image: #imageLiteral(resourceName: "Background1"),
+                                    image: item.fields.linkedAsset(at: "image")?.url ?? URL(string: "")!,
                                     logo: #imageLiteral(resourceName: "Logo1"),
-                                    color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1),
+                                    color: colors.randomElement()!,
                                     show: false))
             }
         }
