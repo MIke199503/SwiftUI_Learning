d09034492bd2a8955ce26094e060300975571b9f
Mike199503 <zth19950326@yahoo.com>
2020-12-15 21:04:46 +0800

    2020-12-15开始第三阶段的学习

---
+++ DesignCode/Buttons.swift
@@ -0,0 +1,47 @@
+//
+//  Buttons.swift
+//  DesignCode
+//
+//  Created by 朱桃禾 on 2020/12/15.
+//
+
+import SwiftUI
+struct Buttons: View {
+    var body: some View {
+        VStack {
+            Text("Button")
+                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                .frame(width: 200, height: 60)
+                .background(
+                    //为了实现内阴影，采用zstack，我的理解就是最后写的在最上面。所以有内阴影
+                    ZStack {
+                        Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1))
+                        
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .foregroundColor(.white)
+                            .blur(radius: 4)
+                            .offset(x: -8, y: -8)
+                        
+                        RoundedRectangle(cornerRadius: 16, style: .continuous)
+                            .fill(
+                                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.9019607843, green: 0.9294117647, blue: 0.9882352941, alpha: 1)), Color.white]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                        )//为了实现一个渐变，所以这里没有使用background，而是使用了一个填充。
+                            .padding(2)
+                            .blur(radius: 2)
+                    }
+                )
+                .clipShape(RoundedRectangle(cornerRadius: 16, style: .continuous))
+                .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+                .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: -20, y: -20)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Buttons_Previews: PreviewProvider {
+    static var previews: some View {
+        Buttons()
+    }
+}
