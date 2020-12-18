7eec74f64b410287fc7e3a75dd439fc7b56bbdef
Mike199503 <zth19950326@yahoo.com>
2020-12-18 22:05:51 +0800

    2020-12-18日提交，修改了支付按钮的动画

--- DesignCode/Buttons.swift
+++ DesignCode/Buttons.swift
@@ -14,6 +14,8 @@ struct Buttons: View {
             
             CircleButton()
             
+            PayButton()
+            
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color(#colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1)))
@@ -136,6 +138,77 @@ struct CircleButton: View {
                         self.tap = false
                     }
                 }
+                .onEnded{ value in
+                    self.press.toggle()
+                }
+        )
+    }
+}
+
+
+
+struct PayButton: View {
+    
+    @GestureState var tap  = false
+    @State var press = false
+    
+    var body: some View {
+        ZStack {
+            Image("fingerprint")
+                .opacity(self.press ? 0:1)
+                .scaleEffect(self.press ? 0 : 1)
+            
+            Image("fingerprint-2")
+                .clipShape(Rectangle().offset(y:tap ? 0 : 50))
+                .animation(.easeOut)
+                .opacity(self.press ? 0:1)
+                .scaleEffect(self.press ? 0 : 1)
+            
+            Image(systemName: "checkmark.circle.fill")
+                .font(.system(size:44,weight:.light))
+                .foregroundColor(Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)))
+                .opacity(self.press ? 1:0)
+                .scaleEffect(self.press ? 1 : 0)
+        }
+        .frame(width:120,height: 120)
+        .background(
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(self.press ? #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), Color(self.press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.8980392157, green: 0.9333333333, blue: 1, alpha: 1))]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                
+                Circle()
+                    .stroke(Color.clear ,lineWidth: 10)
+                    .shadow(color: Color(self.press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 3, x: -5, y: -5)
+                
+                Circle()
+                    .stroke(Color.clear,lineWidth: 10)
+                    .shadow(color: Color(self.press ? #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 3, x: 3, y: 3)
+            }
+        )
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .trim(from: self.tap ? 0.001:1 , to: 1)
+                .stroke(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)), Color(#colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1))]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/),style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .frame(width: 88, height: 88)
+                .rotationEffect(Angle(degrees: 90))
+                .rotation3DEffect(
+                    Angle(degrees: 180),
+                    axis: (x: 1, y: 0.0, z: 0.0)
+                    )
+                .shadow(color: Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)).opacity(0.3), radius: 5, x: 3, y: 3 )
+                .animation(.easeOut)
+        )
+        .shadow(color: Color(self.press ?  #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1) : #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)), radius: 20, x: -20, y: -20)
+        .shadow(color: Color(self.press ? #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) : #colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: 20, y: 20)
+        .scaleEffect(self.tap ? 1.2 : 1)
+        .gesture(
+            LongPressGesture()
+                .updating($tap){ currentState,gestureState, transaction in
+                    //currentstate 当前状态，gestureState动作状态，就是让你当前的动作和你的动效持续。
+                    
+                    gestureState = currentState
+                }
+                
                 .onEnded{ value in
                     self.press.toggle()
                 }
